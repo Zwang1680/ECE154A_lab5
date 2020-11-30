e046d2abadc873b21d53bcfdc38745e2c0edaddd
Zion Wang <zion@ucsb.umail.edu>
2020-11-29 23:58:36 -0800

    second commit

--- mipsmulti.v
+++ mipsmulti.v
@@ -34,7 +34,9 @@ module controller(input       clk, reset,
                   output [1:0] alusrcb, pcsrc,
                   output [2:0] alucontrol);
 
-// **PUT YOUR CODE HERE**
+wire [1:0] aluop;
+maindec md(clk, reset, op, zero, iord, memwrite,  memtoreg, pcen, irwrite, alusrca, alusrcb, regdst, regwrite, aluop);
+aludec ad(funct, aluop, alucontrol);
  
 endmodule
 
@@ -48,7 +50,89 @@ module datapath(input        clk, reset,
                 output        zero,
                 output [31:0] adr, writedata, 
                 input [31:0] readdata);
+    
 
-// **PUT YOUR CODE HERE** 
+endmodule
 
+module maindec(input clk, reset,
+                input [5:0]op, 
+                input zero, 
+                output reg iord, memwrite,  memtoreg, pcen, irwrite, alusrca, 
+                output reg [1:0]alusrcb,
+                output reg regdst, regwrite, aluop);
+
+    wire branch;
+    wire pcwrite;
+    assign pcen = (branch & zero) | pcwrite;
+
+    always @(posedge clk) begin
+        
+    end
+
+endmodule
+
+module aludec(input  [5:0] funct,
+input  [1:0] aluop,
+output reg [2:0] alucontrol);
+always @*
+case(aluop)
+2'b00: alucontrol <= 3'b010; // add (for lw/sw/addi)
+2'b01: alucontrol <= 3'b110; // sub (for beq)
+2'b11: alucontrol <= 3'b001; // or (for ori)
+default: case(funct) // R-type instructions
+6'b100000: alucontrol <= 3'b010; // add
+6'b100010: alucontrol <= 3'b110; // sub
+6'b100100: alucontrol <= 3'b000; // and
+6'b100101: alucontrol <= 3'b001; // or
+6'b101010: alucontrol <= 3'b111; // slt
+default: alucontrol <= 3'bxxx; // ???
+endcase
+endcase
+endmodule
+
+module adder(input  [31:0] a, b,
+output  [31:0] y);
+assign y = a + b;
+endmodule
+
+module sl2(input  [31:0] a,
+output  [31:0] y);
+// shift left by 2
+assign y = {a[29:0], 2'b00};
+endmodule
+
+module signext(input  [15:0] a,
+output  [31:0] y);
+assign y = {{16{a[15]}}, a};
+endmodule
+
+module flopr #(parameter WIDTH = 8)
+(input  clk, reset,
+input  [WIDTH-1:0] d,
+output reg [WIDTH-1:0] q);
+always @(posedge clk, posedge reset)
+if (reset) q <= 0;
+else q <= d;
+endmodule
+
+module mux2 #(parameter WIDTH = 8)
+(input  [WIDTH-1:0] d0, d1,
+input  s,
+output  [WIDTH-1:0] y);
+assign y = s ? d1 : d0;
+endmodule
+
+module mux4 #(parameter WIDTH = 8)
+(
+    input [WIDTH - 1:0] d0, d1, d2, d3,              
+    input [1:0] sel,               
+    output [WIDTH - 1:0] out);
+    assign out = sel[1] ? (sel[0] ? d3 : d2) : (sel[0] ? d1 : d0);
+endmodule
+//We combine the zero extend and the mux, if aluop is 11 then output zeroextend else output srcb
+module zeroextend(input [15:0]a,
+                input [2:0]alucontrol,
+                input [31:0]srcb,
+                output [31:0]b);
+    assign b = (alucontrol == 3'b001) ? {{16{1'b0}}, a} : {srcb};
 endmodule
